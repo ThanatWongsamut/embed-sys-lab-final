58b7cdbbd4f8b426b928bdc047c9917c1b5241e3
ThanatWongsamut <s6107849@mwit.ac.th>
2023-05-20 16:04:07 +0700

    nodemcu: Connect to Wifi & Firebase

--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,15 +1,51 @@
-#define D0 16
-#define LED D0
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#include "WifiCredential.h"
+#include "FirebaseCredential.h"
+
+//Define Firebase Data object
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
 
 void setup()
 {
-  pinMode(LED,OUTPUT);
+  // Start serial
+  Serial.begin(9600);
+
+  // Connect wifi
+  WiFi.begin(SSID, PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting...  ");  
+    Serial.printf("Connection Status: %d\n", WiFi.status()); 
+    delay(1000);
+  }
+  Serial.print("Wi-Fi connected."); 
+  Serial.print("IP Address : ");
+  Serial.println(WiFi.localIP());
+
+  // Firebase client version
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  // Connect Firebase
+  config.api_key = FIREBASE_API_KEY;
+  config.database_url = FIREBASE_DATABASE_URL;
+
+  // Assign the user sign in credentials
+  auth.user.email = FIREBASE_EMAIL;
+  auth.user.password = FIREBASE_PASSWORD;
+  
+  // Assign the callback function for the long running token generation task
+  config.token_status_callback = tokenStatusCallback;
+
+  // Begin with anonymous user
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
 }
 
 void loop()
 {
-  digitalWrite(LED,HIGH);
-  delay(250);
-  digitalWrite(LED,LOW);
-  delay(250);
 }
