a9b698584cff597f9c0f91822e2fd4ff592dc3a7
thanakornnemo <111545764+thanakornnemo@users.noreply.github.com>
2023-05-20 21:56:49 +0700

    STM32: test git

--- STM32/Core/Src/main.c
+++ STM32/Core/Src/main.c
@@ -56,7 +56,7 @@ static void MX_USART2_UART_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int a=0;
 /* USER CODE END 0 */
 
 /**
